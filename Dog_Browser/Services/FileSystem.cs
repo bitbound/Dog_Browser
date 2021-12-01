7d43ae4914f14a9579561d505b3e83f28fc4934a
Jared Goodwin <translucency@outlook.com>
2021-12-01 12:14:49 -0800

    Wire up log viewing.

--- Dog_Browser/Services/FileSystem.cs
+++ Dog_Browser/Services/FileSystem.cs
@@ -15,7 +15,13 @@ namespace Dog_Browser.Services
 
         Stream CreateFile(string filePath);
 
+        bool DirectoryExists(string dirPath);
+
+        IEnumerable<string> EnumerateFiles(string dirPath);
+
         bool FileExists(string filePath);
+        string[] ReadAllLines(string filePath);
+        string ReadAllText(string filePath);
     }
 
     public class FileSystem : IFileSystem
@@ -35,9 +41,29 @@ namespace Dog_Browser.Services
             return File.Create(filePath);
         }
 
+        public bool DirectoryExists(string dirPath)
+        {
+            return Directory.Exists(dirPath);
+        }
+
+        public IEnumerable<string> EnumerateFiles(string dirPath)
+        {
+            return Directory.EnumerateFiles(dirPath);
+        }
+
         public bool FileExists(string filePath)
         {
             return File.Exists(filePath);
+        }
+
+        public string[] ReadAllLines(string filePath)
+        {
+            return File.ReadAllLines(filePath);
+        }
+
+        public string ReadAllText(string filePath)
+        {
+            return File.ReadAllText(filePath);
         }
     }
 }
--- Dog_Browser/ViewModels/LogsPageViewModel.cs
+++ Dog_Browser/ViewModels/LogsPageViewModel.cs
@@ -1,13 +1,94 @@
 ﻿using Dog_Browser.Mvvm;
+using Dog_Browser.Services;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Dog_Browser.ViewModels
 {
     public class LogsPageViewModel : ObservableObject
     {
+        private readonly IDialogService _dialogService;
+        private readonly IFileSystem _fileSystem;
+        private readonly ILogger<LogsPageViewModel> _logger;
+        private readonly string _logsDirectory = Path.Combine(Path.GetTempPath(), "Dog_Browser");
+        private string _currentLogContents = "";
+        private string _selectedLogName = "";
+        public LogsPageViewModel(IFileSystem fileSystem, IDialogService dialogService, ILogger<LogsPageViewModel> logger)
+        {
+            _fileSystem = fileSystem;
+            _dialogService = dialogService;
+            _logger = logger;
+
+            LoadLogFileNames();
+        }
+
+        public string CurrentLogContents
+        {
+            get => _currentLogContents;
+            set => SetProperty(ref _currentLogContents, value);
+        }
+
+        public ObservableCollection<string> LogNames { get; } = new();
+
+        public string SelectedLogName
+        {
+            get => _selectedLogName;
+            set
+            {
+                SetProperty(ref _selectedLogName, value);
+                LoadLogFileContents();
+            }
+        }
+
+        private void LoadLogFileContents()
+        {
+            try
+            {
+                var filePath = Path.Combine(_logsDirectory, SelectedLogName);
+                CurrentLogContents = _fileSystem.ReadAllText(filePath);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading log file.");
+                _dialogService.Show("There was an error loading the log file.", "Loading Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void LoadLogFileNames()
+        {
+            try
+            {
+                if (!_fileSystem.DirectoryExists(_logsDirectory))
+                {
+                    return;
+                }
+
+                var files = _fileSystem.EnumerateFiles(_logsDirectory);
+
+                if (!files.Any())
+                {
+                    return;
+                }
+
+                LogNames.Clear();
+                foreach (var file in files)
+                {
+                    LogNames.Add(Path.GetFileName(file));
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error loading log file names.");
+                _dialogService.Show("There was an error loading the log file names.", "Loading Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
